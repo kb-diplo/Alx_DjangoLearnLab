c0f28a142522163ae4bcb466d07a43da315d9913
Mbugualawrencee <143984474+kb-diplo@users.noreply.github.com>
2025-03-10 07:53:05 +0300

    Update views.py to inlude booklist link api

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,14 @@
-from rest_framework import viewsets, permissions
+from rest_framework import generics,viewsets, permissions
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+# Create your views here.
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [AllowAny]  #Allow any user to access
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
