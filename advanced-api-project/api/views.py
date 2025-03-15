494e4c791b9eeda290b75c5db638e91ec6bb7e60
LARRY <tingzlarry@gmail.com>
2025-03-15 09:41:49 +0300

    Fixed NameError by importing AllowAny and IsAuthenticated

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 # BookListView: Retrieve all books (public access)
 class BookListView(generics.ListAPIView):
