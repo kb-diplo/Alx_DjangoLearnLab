1049251bc874d99c854f8ef1bd09162f54308536
LARRY <tingzlarry@gmail.com>
2025-03-15 09:20:44 +0300

    Initial commit: Set up Django project with custom serializers

---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,21 @@
+# BookSerializer: Serializes all fields of the Book model and validates the publication year.
+from rest_framework import serializers
+from .models import Book, Author
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'publication_year', 'author']
+
+    def validate_publication_year(self, value):
+        if value > 2023:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
+
+# AuthorSerializer: Serializes the Author model and includes nested BookSerializer for related books.
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Author
+        fields = ['id', 'name', 'books']
