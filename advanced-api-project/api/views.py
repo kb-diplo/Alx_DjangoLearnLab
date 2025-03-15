fc165c9aca7492dd1fce3338a444ad44ed433764
LARRY <tingzlarry@gmail.com>
2025-03-15 10:27:36 +0300

    Fixed SearchFilter and OrderingFilter setup

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,12 +1,13 @@
 from rest_framework import generics
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.response import Response
 from rest_framework import status 
 from django_filters import rest_framework
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated ,AllowAny
-from rest_framework.filters import OrderingFilter ,SearchFilter
-from django_filters.rest_framework import DjangoFilterBackend 
+
 
 # BookListView: Retrieve all books (public access)
 class BookListView(generics.ListAPIView):
