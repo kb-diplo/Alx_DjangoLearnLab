f65bdbf65d8ef8d41df407375b3ba31ccd5c3720
LARRY <tingzlarry@gmail.com>
2025-03-15 10:32:30 +0300

    Fixed OrderingFilter setup using filters.OrderingFilter

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -21,7 +21,7 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [AllowAny]
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter] 
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter] 
     filterset_fields = ['title', 'author__name', 'publication_year']
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year'] 
