fc1282257e675f16ae943ddb30b40cd54c0051ed
Mbugualawrencee <143984474+kb-diplo@users.noreply.github.com>
2025-03-18 09:12:39 +0300

    Django REST Framework’s permission classes to protect API endpoints based on user roles.

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics,viewsets, permissions
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, AllowAny
 
 # Create your views here.
 
