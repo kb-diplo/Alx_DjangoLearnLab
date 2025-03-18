efe7cbd49737af18d26739995317b7081a2a232f
LARRY <tingzlarry@gmail.com>
2025-03-18 11:36:24 +0300

    Configured Django to use MySQL database

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'django_blog_db',  
+        'USER': 'larry',  
+        'PASSWORD': 'larry',  
+        'HOST': 'localhost',  
+        'PORT': '3306',  
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
