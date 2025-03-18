043dc12c45d24cc46e209601ed0cd77d0d5de969
LARRY <tingzlarry@gmail.com>
2025-03-18 13:53:15 +0300

    Added TagWidget, search functionality, and URL configuration

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Comment, Post
+from taggit.forms import TagWidget
 
 class RegisterForm(UserCreationForm):
     # Add an email field to the default UserCreationForm
@@ -19,4 +20,9 @@ class CommentForm(forms.ModelForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tags']  
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),  # Use TagWidget for the tags field
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+        }
